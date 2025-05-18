122d9bf53e9341b0ccdf533f1226f2cb4127fd0b
adamsjt13 <adamsjt13@gmail.com>
2025-05-18 15:04:04 -0500

    adding struct for display options and later command line args

--- hexdump/hexdump.c
+++ hexdump/hexdump.c
@@ -2,24 +2,33 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-#define BYTES_PER_LINE 16
+// structure to hold display options, command line args
+typedef struct {
+    int bytes_per_line; // default 16, controls number of bytes printed per line
+    int group_size; // default 8, controls spacing between groups
+    int show_ascii; // show ASCII at the end
+} DisplayOptions;
 
 void print_usage(const char *program_name);
-void print_hex_line(const unsigned char *buffer, size_t bytes_read, size_t offset);
-void print_hex_values(const unsigned char *buffer, size_t bytes_read);
+void print_hex_line(const unsigned char *buffer, size_t bytes_read, size_t offset, const DisplayOptions *opts);
+void print_hex_values(const unsigned char *buffer, size_t bytes_read, const DisplayOptions *opts);
 void print_ascii_representation(const unsigned char *buffer, size_t bytes_read);
-int process_file(const char *filename);
+int process_file(const char *filename, const DisplayOptions *opts);
 
 void print_usage(const char *program_name) {
     fprintf(stderr, "Usage: %s <file>\n", program_name);
 }
 
-void print_hex_values(const unsigned char *buffer, size_t bytes_read) {
-    for (size_t i = 0; i < BYTES_PER_LINE; i++) {
+void print_hex_values(const unsigned char *buffer, size_t bytes_read, const DisplayOptions *opts) {
+    for (size_t i = 0; i < opts->bytes_per_line; i++) {
         if (i < bytes_read){
             printf("%02x ", buffer[i]);
         } else {
             printf("   ");
+        }
+        // add space between groups
+        if (i % opts->group_size){
+            printf(" ");
         }
     }
 }
@@ -32,24 +41,27 @@ void print_ascii_representation(const unsigned char *buffer, size_t bytes_read) 
     printf("|");
 } 
 
-void print_hex_line(const unsigned char *buffer, size_t bytes_read, size_t offset) {
-    printf("%08lx   ", offset);
+void print_hex_line(const unsigned char *buffer, size_t bytes_read, size_t offset, const DisplayOptions *opts) {
+    printf("%08lx  ", offset);
 
-    print_hex_values(buffer, bytes_read);
+    print_hex_values(buffer, bytes_read, opts);
 
-    print_ascii_representation(buffer, bytes_read);
+    if (opts->show_ascii){
+        printf(" ");
+        print_ascii_representation(buffer, bytes_read);
+    }
 
     printf("\n");
 }
 
-int process_file(const char *filename) {
+int process_file(const char *filename, const DisplayOptions *opts) {
     FILE *fp = fopen(filename, "rb");
     if (fp == NULL) {
         fprintf(stderr, "Error: Can't open %s\n", filename);
         return 1;
     }
 
-    unsigned char *buffer = malloc(BYTES_PER_LINE * sizeof(unsigned char));
+    unsigned char *buffer = malloc(opts->bytes_per_line * sizeof(unsigned char));
     if (buffer == NULL) {
         fprintf(stderr, "Error: Memory allocation failed\n");
         fclose(fp);
@@ -59,8 +71,8 @@ int process_file(const char *filename) {
     size_t bytes_read;
     size_t offset = 0;
 
-    while((bytes_read = fread(buffer, 1, BYTES_PER_LINE, fp)) > 0) {
-        print_hex_line(buffer, bytes_read, offset);
+    while((bytes_read = fread(buffer, 1, opts->bytes_per_line, fp)) > 0) {
+        print_hex_line(buffer, bytes_read, offset, opts);
         offset += bytes_read;
     }
 
@@ -85,5 +97,11 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    return process_file(argv[1]);
+    DisplayOptions opts = {
+        .bytes_per_line = 8,
+        .group_size = 2,
+        .show_ascii = 0
+    };
+
+    return process_file(argv[1], &opts);
 }
