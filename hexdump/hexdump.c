dec68be1aa0d1838fb23ee934d783e23838eaf33
adamsjt13 <adamsjt13@gmail.com>
2025-05-18 11:22:56 -0500

    reading file and printing to command prompt

--- hexdump/hexdump.c
+++ hexdump/hexdump.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+    // open file
+    FILE *fp;
+    void fileprint(FILE *);
+    char *prog = argv[0];
+
+    if (argc == 1){
+        fileprint(stdin);
+    }
+    else {
+        while (--argc > 0){
+            if ((fp = fopen(*++argv, "rb")) == NULL){
+                fprintf(
+                    stderr,
+                    "%s: can't open %s\n",
+                    prog,
+                    *argv
+                );
+                return 1;
+            }
+            else {
+                fileprint(fp);
+                fclose(fp);
+            }
+        }
+    }
+
+    if (ferror(stdout)){
+        fprintf(
+            stderr,
+            "%s: error writing stdout\n",
+            prog);
+        return 2;
+    }
     return 0;
+}
+
+void fileprint(FILE *ifp){
+    int c;
+
+    while((c = getc(ifp)) != EOF){
+        putchar(c);
+    }
+
+    printf("\n");
 }
